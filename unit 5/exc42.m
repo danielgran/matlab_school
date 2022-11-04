ff17f92b7b4089bca5a34ddceba1113a3183e78a
Daniel Gran <contact@grandaniel.com>
2022-11-04 11:07:11 +0100

    Changed: Exc

--- unit 5/exc42.m
+++ unit 5/exc42.m
@@ -9,5 +9,15 @@ a=1-1/sqrt(1-D^2)*exp(-D*w0*t).*sin(w0*sqrt(1-D^2)*t+PHI);
 einh_o=1+1/sqrt(1-D^2)*exp(-D*w0*t)
 einh_u=1-1/sqrt(1-D^2)*exp(-D*w0*t)
 
+[amax,xmax]=max(a)
+xmax = t(xmax)
+
 plot(t,a,'k',t,einh_o,'k--', t, einh_u, 'k--')
+plot(xmax,amax, "Maximalwert")
+ylim([-0.3,2.3])
+xlabel("t\rightarrow")
+ylabel("a(t)\rightarrow")
+
+
+
 grid on
