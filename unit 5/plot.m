ef1a8e8008cb5152174e472e4119b84496a9f87e
Daniel Gran <contact@grandaniel.com>
2022-11-04 10:25:22 +0100

    Added plot functionality

---
+++ unit 5/plot.m
@@ -0,0 +1,8 @@
+x=[1,2,3]
+y=[1,2,3]
+
+plot(x,y,x,y)
+figure(2) % creates a new figure window using default property values. The resulting figure is the current figure.
+xlabel("x\rightarrow")
+ylabel("y\rightarrow")
+title("Meine Funktion")
